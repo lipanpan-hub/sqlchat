4ddc544104dffebe36ba9b0fc96705e6207c6924
lipanpan-hub <47287977+lipanpan-hub@users.noreply.github.com>
2023-04-15 21:43:04 +0800

    删掉引流内容

--- src/components/ConversationView/Header.tsx
+++ src/components/ConversationView/Header.tsx
@@ -26,7 +26,7 @@ const Header = (props: Props) => {
         className || ""
       } w-full flex flex-row justify-between items-center lg:grid lg:grid-cols-3 py-1 border-b dark:border-zinc-700 z-1`}
     >
-      <div className="ml-2 flex justify-start items-center">
+      {/* <div className="ml-2 flex justify-start items-center">
         <button
           className="w-8 h-8 p-1 mr-1 block lg:hidden rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-zinc-700"
           onClick={() => layoutStore.toggleSidebar()}
@@ -35,9 +35,9 @@ const Header = (props: Props) => {
         </button>
         <span className="w-auto text-left block lg:hidden">{title}</span>
         <GitHubStarBadge className="hidden lg:flex ml-2" />
-      </div>
+      </div> */}
       <span className="w-auto text-center h-8 p-1 hidden lg:block">{title}</span>
-      <div className="mr-2 sm:mr-3 relative flex flex-row justify-end items-center">
+      {/* <div className="mr-2 sm:mr-3 relative flex flex-row justify-end items-center">
         <a
           href="https://www.bytebase.com?source=sqlchat"
           className="flex flex-row justify-center items-center h-10 px-3 py-1 rounded-md whitespace-nowrap hover:bg-gray-100 dark:hover:bg-zinc-700"
@@ -45,7 +45,7 @@ const Header = (props: Props) => {
         >
           <img className="h-5 sm:h-6 w-auto" src={isDarkMode ? "/craft-by-bytebase-dark-mode.webp" : "/craft-by-bytebase.webp"} alt="" />
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
